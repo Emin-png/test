ca6fd5f69281466447430506aad67a657eddb4dc
Emin-png <emin_gardashov@yahoo.co.uk>
2020-05-09 17:06:10 +0400

    newes version

--- app2.js
+++
@@ -1,9 +0,0 @@
-var express=require('express');
-var app=express();
-app.get('/',function(req,res){
-    res.send("This is home");
-});
-app.get('/news',function(req,res){
-    res.send("This is news");
-});
-app.listen(3000);
